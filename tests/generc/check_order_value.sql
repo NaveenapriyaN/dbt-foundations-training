7b7fdf5ef3f69174a82ff6d3ba52b0d0c02ce75d
NAVEENA PRIYA N <naveenapriya13@gmail.com>
2026-09-11 06:39:54 +0000

    Removed hardcoded values in tests

--- tests/generc/check_order_value.sql
+++ tests/generc/check_order_value.sql
@@ -2,7 +2,7 @@
 
 select order_id, total_price
 from {{ ref('stg_orders') }}
-where total_price < 800
+where (total_price < {{var('v_min_order')}})
 
 -- select order_id, total_price ordertotal 
 -- from analytics.dbt_murali.stg_orders where (ordertotal<=880);
